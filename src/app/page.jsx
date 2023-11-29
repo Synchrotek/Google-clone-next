52ea24775ab6929df4217d5628ad353329931c18
Satyajit Nayak <satyajitnayak348@gmail.com>
2023-11-29 19:23:49 +0530

    added footer component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -14,6 +14,7 @@ export default function Home() {
           width="300"
           height="100"
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/2560px-Google_2015_logo.svg.png"
+          alt="Google Logo"
         />
         <HomeSearch />
       </div>
--- src/components/CountryLookup.jsx
+++ src/components/CountryLookup.jsx
@@ -5,10 +5,9 @@ export default function CountryLookup() {
     const [country, setCountry] = useState("Italy");
 
     useEffect(() => {
-        fetch(`https://https://extreme-ip-lookup.com/json/?key=${process.env.NEXT_PUBLIC_IP_API_KEY}`)
+        fetch(`https://extreme-ip-lookup.com/json/?key=${process.env.NEXT_PUBLIC_IP_API_KEY}`)
             .then((res) => res.json())
             .then((data) => setCountry(data.country));
-        console.log(process.env.IP_API_KEY);
     }, [])
 
     return (<div>{country}</div>)

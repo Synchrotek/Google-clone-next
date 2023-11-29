acc4b0a52914dbd322a24a6fe9eba1936a60bbec
Satyajit Nayak <satyajitnayak348@gmail.com>
2023-11-29 20:07:17 +0530

    update searchHeader

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -13,7 +13,7 @@ export default function Home() {
         <Image
           width="300"
           height="100"
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/2560px-Google_2015_logo.svg.png"
+          src="/GoogleLogo.png"
           alt="Google Logo"
         />
         <HomeSearch />
--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -1,6 +1,29 @@
+import Link from "next/link"
+import Image from "next/image"
+import SearchBox from "./SearchBox"
+import SearchHeaderOptions from "./SearchHeaderOptions";
+import { RiSettings3Line } from "react-icons/ri";
+import { TbGridDots } from "react-icons/tb"
 
 export default function SearchHeader() {
-    return (
-        <div>SearchHeader</div>
+    return (<header className="sticky top-0 bg-white">
+        <div className="flex w-full p-6 items-center justify-between">
+            <Link href={"/"}>
+                <Image width="120" height="40"
+                    src={"/GoogleLogo.png"}
+                    alt="Google Logo"
+                />
+            </Link>
+            <div className="flex-1">
+                <SearchBox />
+            </div>
+            <div className="hidden md:inline-flex space-x-2">
+                <RiSettings3Line className="header-icon" />
+                <TbGridDots className="header-icon" />
+            </div>
+            <button className="bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md transition-all ml-2">Sign in</button>
+        </div>
+        <SearchHeaderOptions />
+    </header>
     )
 }

7990958a9a04a1726ed1029a058339bd7044346b
Satyajit Nayak <satyajitnayak348@gmail.com>
2023-11-29 17:35:04 +0530

    updated readme.md

--- src/components/CountryLookup.jsx
+++ src/components/CountryLookup.jsx
@@ -1,6 +1,15 @@
+"use client"
+import { useEffect, useState } from "react"
 
 export default function CountryLookup() {
-    return (
-        <div>CountryLookup</div>
-    )
+    const [country, setCountry] = useState("Italy");
+
+    useEffect(() => {
+        fetch(`https://https://extreme-ip-lookup.com/json/?key=${process.env.NEXT_PUBLIC_IP_API_KEY}`)
+            .then((res) => res.json())
+            .then((data) => setCountry(data.country));
+        console.log(process.env.IP_API_KEY);
+    }, [])
+
+    return (<div>{country}</div>)
 }

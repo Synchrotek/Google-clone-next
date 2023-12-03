934c3a1d9d08292b433e94c9d8f0f89447b305ff
Satyajit Nayak <satyajitnayak348@gmail.com>
2023-12-03 15:34:50 +0530

    google search api data fetch

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
 
-export default function WebSearchPage() {
-    return (
-        <div>WebSearchPage</div>
+export default async function WebSearchPage({ searchParams }) {
+
+    const response = await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLESEARCH_API_KEY}&cx=${process.env.GOOGLESEARCH_CONTEXT_KEY}&q=${searchParams.searchTerm}`
+    );
+
+    const data = await response.json();
+    console.log("Fetched the Google-API Again");
+
+    const results = data.items;
+
+    return (<>
+        {results && results.map(result =>
+            <h1>{result.title}</h1>
+        )}
+    </>
     )
 }

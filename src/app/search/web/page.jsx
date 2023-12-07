8f9e86c4a5a08c4402682d4b918fed6b2e43a39f
Satyajit Nayak <satyajitnayak348@gmail.com>
2023-12-07 17:29:55 +0530

    created SearchResultComponent

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,8 +1,10 @@
 import Link from 'next/link';
 import React from 'react'
+import WebSearchResults from '@/components/WebSearchResults';
 
 export default async function WebSearchPage({ searchParams }) {
 
+    await new Promise((resolve) => setTimeout(resolve, 10000));
     const response = await fetch(
         `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLESEARCH_API_KEY}&cx=${process.env.GOOGLESEARCH_CONTEXT_KEY}&q=${searchParams.searchTerm}`
     );
@@ -21,14 +23,13 @@ export default async function WebSearchPage({ searchParams }) {
             <h1 className='text-3xl mb-4'>No results found</h1>
             <p>Try searching for somethign else or go back to the homepage</p>
             <Link href={"/"} className='text-blue-500'>Home</Link>
+            <Link href={"/"} className='text-blue-500'>Home</Link>
         </div>
         )
     }
 
     return (<>
-        {results && results.map(result =>
-            <h1>{result.title}</h1>
-        )}
+        {results && <WebSearchResults results={data} />}
     </>
     )
 }
